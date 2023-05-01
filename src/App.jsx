1f3e470cf4e4f252148ff578b906c21c46ff9f98
Gungz <gungz87@gmail.com>
2023-05-01 20:08:18 +0700

    Blog post generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Configuration, OpenAIApi } from "openai";
-import cadenaLogo from './assets/cadenaLogo.png';
 
 //open ai config
 const configuration = new Configuration({
@@ -11,6 +10,7 @@ const apiClient = new OpenAIApi(configuration);
 const App = () => {
   const [prompt, setPrompt] = useState("");
   const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState("invisible");
 
   const handlePromptChange = (event) => {
     setPrompt(event.target.value);
@@ -22,16 +22,18 @@ const App = () => {
     event.preventDefault();
 
     try {
+      setLoading("visible");
       const completions = await apiClient.createCompletion({
         model: "text-davinci-003",
-        prompt: `Customize Your Prompt Here: ${prompt}`,
-        max_tokens: 880,
-        temperature: 0.7,
+        prompt: `Generate lengthy blog post for this topic: ${prompt}`,
+        max_tokens: 3000,
+        temperature: 0.8,
         top_p: 1,
-        frequency_penalty: 0,
+        frequency_penalty: 0.2,
         presence_penalty: 0,
       });
       console.log(completions);
+      setLoading("invisible");
       setResponse(completions.data.choices[0].text);
     } catch (error) {
       console.log(error);
@@ -40,18 +42,26 @@ const App = () => {
 
   return (
     <div className="flex flex-col items-center justify-center px-4 sm:p-0">
-      <img src={cadenaLogo} alt="Cadena Logo" className="h-12 mb-10 mt-40" />
+      <div role="status" className={loading}>
+        <svg aria-hidden="true" class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+        <span class="sr-only">Loading...</span>
+      </div>
+      <div className="h-12 mb-10 mt-40 text-white text-4xl font-extrabold">Your Blog Post Generator</div>
+    
       <form onSubmit={handleSubmit} className="w-full max-w-lg">
         <div className="flex items-center border-b-2 border-indigo-600 py-2">
           <input
             className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
-            placeholder="Enter a topic you want to tweet about."
+            placeholder="Enter a topic you want to generate blog post about."
             value={prompt}
             onChange={handlePromptChange}
           />
-          <button
-            className="flex-shrink-0 bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-sm border-4 text-white py-1 px-2 rounded"
+          <button disabled={loading == "visible"}
+            className={`flex-shrink-0 bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-sm border-4 text-white py-1 px-2 rounded ${loading == "visible" ? "disabled:opacity-25" : ""}`}
             type="submit"
           >
             Generate
